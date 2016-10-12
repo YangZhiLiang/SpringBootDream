4df9251860d5d6bcee68161537d4eb8e417cb774
DreamYoung <yangzhiliang1018@gmail.com>
2016-10-12 11:35:03 +0800

    add kotlin and banner

--- swagger/src/main/java/dream/young/springboot/swagger/SwaggerApplication.java
+++ swagger/src/main/java/dream/young/springboot/swagger/SwaggerApplication.java
@@ -7,12 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Desc:
  * Mail: yangzl@terminus.io
  * author: DreamYoung
- * Date: 2016/10/11
+ * Date: 2016/10/12
  */
 @SpringBootApplication
 public class SwaggerApplication {
 
+    private static final String BANNER = "  _____                            __     __                     \n" +
+            " |  __ \\                           \\ \\   / /                     \n" +
+            " | |  | |_ __ ___  __ _ _ __ ___    \\ \\_/ /__  _   _ _ __   __ _ \n" +
+            " | |  | | '__/ _ \\/ _` | '_ ` _ \\    \\   / _ \\| | | | '_ \\ / _` |\n" +
+            " | |__| | | |  __/ (_| | | | | | |    | | (_) | |_| | | | | (_| |\n" +
+            " |_____/|_|  \\___|\\__,_|_| |_| |_|    |_|\\___/ \\__,_|_| |_|\\__, |\n" +
+            "                                                            __/ |\n" +
+            "                                                           |___/ \n";
+
     public static void main(String[] args) {
-        SpringApplication.run(SwaggerApplication.class, args);
+        SpringApplication application = new SpringApplication(SwaggerApplication.class);
+        application.setBanner((environment, aClass, printStream) -> printStream.print(BANNER));
+        application.run(args);
     }
 }
